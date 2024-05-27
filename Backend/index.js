d9aecef0223bed98a81292bc114bc62c559e5e19
shraddhabyloop24 <shraddhapatel4829907@gmail.com>
2024-05-27 11:35:19 +0530

    Update

--- Backend/index.js
+++ Backend/index.js
@@ -199,7 +199,7 @@ app.delete('/addfoods/:id', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
