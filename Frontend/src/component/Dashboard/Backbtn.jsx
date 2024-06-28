9bb085343ac7c2241435b28e208873bf451bdcc6
shraddhabyloop24 <shraddhapatel4829907@gmail.com>
2024-06-28 12:53:58 +0530

    update backbtn

--- Frontend/src/component/Dashboard/Backbtn.jsx
+++ Frontend/src/component/Dashboard/Backbtn.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Backbtn = () => {
   return (
     <>
-    <div class="page-wrapper">
-     <div class="page-breadcrumb">
-    <div class="row">
-      <div class="col-12 d-flex no-block align-items-center">
-        <h4 class="page-title">Dashboard</h4>
-        <div class="ms-auto text-end">
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active" aria-current="page">
-                Library
-              </li>
-            </ol>
-          </nav>
+      <div class="page-wrapper">
+        <div class="page-breadcrumb">
+          <div class="row">
+            <div class="col-12 d-flex no-block align-items-center">
+              <h4 class="page-title">Dashboard</h4>
+              <div class="ms-auto text-end">
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                      <Link to="/Dashboard">
+                      Home  <i className="fa-solid fa-chevron-right"></i>
+                      </Link>
+                    </li>
+
+                    <li class="breadcrumb-item ">
+                      Library
+                    </li>
+                  </ol>
+                </nav>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  </div>
     </>
   )
 }
